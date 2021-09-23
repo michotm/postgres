63ac83fdf925c108644c9f1926ce21f2a83aefe6
Michael Michot <michael.michot@pharmasimple.com>
2021-09-23 14:29:53 +0200

    [FIX] replication access + ssh key

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-FROM postgres:13.4
+FROM postgres:13.2
 RUN localedef -i fr_BE -c -f UTF-8 -A /usr/share/locale/locale.alias fr_BE.UTF-8
-ENV LANG=fr_BE.utf8
+ENV LANG=en_US.utf8
 RUN apt-get update && apt-get install -y locales barman-cli
 COPY initdb.d/* /docker-entrypoint-initdb.d
+COPY .ssh/* /var/lib/postgresql/.ssh/
+RUN chown -R postgres:postgres /var/lib/postgresql/.ssh
+
--- initdb.d/init-user-db.sh
+++ initdb.d/init-user-db.sh
@@ -2,6 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE ROLE barman WITH SUPERUSER PASSWORD '${BARMAN_PASSWORD}';
+    CREATE ROLE barman WITH LOGIN SUPERUSER PASSWORD '${BARMAN_PASSWORD}';
     GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO barman;
 EOSQL
+
+echo "host replication all all md5" >> /var/lib/postgresql/data/pg_hba.conf
